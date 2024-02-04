f5167725e8a5b33a2b7a489298890503fedf3da9
gregory-code <111695375+gregory-code@users.noreply.github.com>
2024-02-04 12:16:14 -0600

    Created Project
    First Commit to main

---
+++ Source/diablo/Private/EnemyCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterBase.h"
+#include "EnemyCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEnemyCharacter : public ACharacterBase
+{
+	GENERATED_BODY()
+	
+};
