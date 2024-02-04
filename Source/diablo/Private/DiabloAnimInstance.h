78026eb1318c8fedc4048cf9e1ffcb266ba1b3ea
gregory-code <111695375+gregory-code@users.noreply.github.com>
2024-02-04 13:57:27 -0600

    Created Scripts
    Did some setup

---
+++ Source/diablo/Private/DiabloAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "DiabloAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UDiabloAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
